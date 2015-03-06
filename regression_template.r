f85125c1c0608a98fc4a589dab31caa5ed35189b
John PatricK <jspatrick@gmail.com>
2015-03-05 19:53:52 -0800

    dennis's updates

--- regression_template.r
+++ regression_template.r
@@ -1,45 +1,58 @@
-company_data <- read.csv("data_mat2.txt")
+company_data <- read.csv("prefix2.csv")
 attach(company_data)
 
 #Segment the Data
-APPLE <- subset(company_data, Company=="APPLE", select=Company:Gasprice)
-APPLE_SubSample <- sample(APPLE)
-BESTBUY <- subset(company_data, Company=="BEST BUY", select=Company:Gasprice)
-H&M <- subset(company_data, Company=="H&M", select=Company:Gasprice)
-WALMART <- subset(company_data, Company=="WALMART", select=Company:Gasprice)
-PETCO <- subset(company_data, Company=="PETCO", select=Company:Gasprice)
-MCDONALDS <- subset(company_data, Company=="MCDONALD'S", select=Company:Gasprice)
-SHELL <- subset(company_data, Company=="SHELL", select=Company:Gasprice)
+APPLE <- subset(company_data, Company=="APPLE", select=Company:Month)
+#NEED TO CREATE HOLDOUTS
+BESTBUY <- subset(company_data, Company=="BEST BUY", select=Company:Month)
+H&M <- subset(company_data, Company=="H&M", select=Company:Month)
+WALMART <- subset(company_data, Company=="WALMART", select=Company:Month)
+PETCO <- subset(company_data, Company=="PETCO", select=Company:Month)
+MCDONALDS <- subset(company_data, Company=="MCDONALD'S", select=Company:Month)
+SHELL <- subset(company_data, Company=="SHELL", select=Company:Month)
 
 
 #Runthrough of Regression with specific company -> Set targetco = COMPANY_NEEDED
-targetco <- APPLE
+targetco <- WALMART
 
-#Create dummies for weeks for top 5 retail (Black Friday, etc)
-Byweek <- factor(targetco$Week.Number)
-weekdummy <- model.matrix(~Byweek)
+#Create dummies for Month
+ByMonth <- factor(targetco$Month)
+Monthdummy <- model.matrix(~ByMonth)
 
 #Create Dummy Variables bucketing into Regions
-Zip <- factor(targetco$Zip.Code.Prefix)
-zipdummy <- model.matrix(~Zip)
+#Zip <- factor(targetco$Zip.Prefix)
+#zipdummy <- model.matrix(~Zip)
 
 #Create Dummy Variables for each integer of Temp
-Temp <- factor(as.integer(targetco$Temperature/10))
-tempdummy <- model.matrix(~Temp)
+#Temps <- factor(as.integer(targetco$Temp/10))
+#tempdummy <- model.matrix(~Temps)
+
+#Create Dummy Variables for month
+
 
 #Create a Squared Temperature
-tempsquared <- targetco$Temperature*targetco$Temperature
+tempsquared <- targetco$Temp*targetco$Temp
 
-#Regression time
-regress = lm(targetco$Sales~ targetco$Precipitation + 
-               targetco$SearchTrends + 
-               targetco$SearchTrendsLaggedby1 + 
-               targetco$Week.Number+ 
-               targetco$Temperature + 
-               tempsquared)
+#Interactives
+#tempbyregion <- targetco$Temperature*targetco$Region
+#precipbyregion <- targetco$Precipitation*targetco$Region
+onlinebysearch <- targetco$Non.Physical.Sales*targetco$GoogTrend
+onlinebylagged <- targetco$Non.Physical.Sales*targetco$GoogTrendM1
+
+#Regression time - NEED TO ADD REGION, temp*region, precip*region
+regress = lm(targetco$Sales~ targetco$Temp + 
+               tempsquared + 
+               targetco$Precip + 
+               targetco$Non.Physical.Sales +
+               Monthdummy +
+               targetco$Gprice +
+               targetco$GoogTrend + 
+               targetco$GoogTrendM1 + 
+               onlinebysearch +
+               onlinebylagged
+)
 summary(regress)
 plot(regress$residuals)
 
 #Graphing
-plot(targetco$Temperature, targetco$Sales)
-
+plot(targetco$Temp, targetco$Non.Physical.Sales)
